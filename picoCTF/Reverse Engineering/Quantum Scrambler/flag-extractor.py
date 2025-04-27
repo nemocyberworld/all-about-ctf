4ff3a5a1c0cff5588c7f66976d18203b6c04b3b3
Captain Nemo <nemo14398@gmail.com>
2025-04-27 12:35:06 +0600

    Delete picoCTF/Reverse Engineering/Quantum Scrambler directory

--- picoCTF/Reverse Engineering/Quantum Scrambler/flag-extractor.py
+++
@@ -1,3 +0,0 @@
-cypher = // include here the long string from netcat then extract the flag from first line, from hex to ascii
-for x in range(len(cypher)):
-    print(cypher[x][0], cypher[x][-1], end="")
